a5e825bae23fb368a987b5d5e640147c66357186
Cherepakhin Vasili <vasi.che@gmail.com>
2019-12-21 13:31:00 +0500

    Проверка почты

--- src/main/java/ru/dfsystems/savepost/controller/SavePostController.java
+++ src/main/java/ru/dfsystems/savepost/controller/SavePostController.java
@@ -1,7 +1,5 @@
 package ru.dfsystems.savepost.controller;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,24 +10,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 @RestController
 @RequestMapping("/log")
 public class SavePostController {
     public Logger logger = LoggerFactory.getLogger(SavePostController.class);
 
-    @Value( "${logging.file}" )
+    @Value("${logging.file}")
     private String loggingfile;
 
     @RequestMapping(value = "", method = RequestMethod.GET)
     public ResponseEntity<String> get() throws Exception {
         String content = "";
-        content = new String ( Files.readAllBytes( Paths.get(loggingfile) ) );
+        content = new String(Files.readAllBytes(Paths.get(loggingfile)));
         return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON_UTF8).body(content);
     }
 
     @RequestMapping(value = "", method = RequestMethod.POST)
     public ResponseEntity<String> create(@RequestBody String req) throws Exception {
-        logger.info("{\"POST\":" + req+"}\n");
+        logger.info("{\"POST\":" + req + "}\n");
         return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON_UTF8).body("");
     }
 }
