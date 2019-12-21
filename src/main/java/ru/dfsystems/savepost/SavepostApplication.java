f7cd171adff188a3af391abe84ca32a9ade7c442
Cherepakhin Vasili <vasi.che@gmail.com>
2019-12-21 13:34:30 +0500

    Проверка почты

--- src/main/java/ru/dfsystems/savepost/SavepostApplication.java
+++ src/main/java/ru/dfsystems/savepost/SavepostApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SavepostApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SavepostApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SavepostApplication.class, args);
+    }
 
 }
